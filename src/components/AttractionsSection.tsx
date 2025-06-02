2b99aa6b07c28d334557def61a4def77d96d552a
Yuttakhan B <yuttakhanbaingen@gmail.com>
2025-06-02 15:26:19 +0700

    feat: apply data to AttractionsSection

--- src/components/AttractionsSection.tsx
+++ src/components/AttractionsSection.tsx
@@ -5,6 +5,8 @@ import { Link } from 'react-router-dom';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent } from '@/components/ui/card';
 import { useLanguage } from '@/context/LanguageContext';
+import { useGetPlaces } from '@/hooks/api/useGetPlaces';
+import { Skeleton } from './ui/skeleton';
 
 const attractions = [
   {
@@ -60,6 +62,8 @@ const attractions = [
 const AttractionsSection: React.FC = () => {
   const { t, language } = useLanguage();
 
+  const { data, isLoading } = useGetPlaces();
+
   return (
     <section className="py-16 bg-gray-50">
       <div className="container">
@@ -74,24 +78,26 @@ const AttractionsSection: React.FC = () => {
         </div>
         
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-          {attractions.map((attraction) => (
+          {!isLoading && data.data.length > 0 && data.data.map((attraction) => (
             <Link to={`/attractions/${attraction.id}`} key={attraction.id}>
               <Card className="overflow-hidden card-hover">
                 <div className="h-52 overflow-hidden">
-                  <img 
-                    src={attraction.image} 
-                    alt={attraction.name[language as keyof typeof attraction.name]} 
+                  {/* TODO: Add image */}
+                  {/* <img 
+                    src={attraction.images[0]} 
+                    alt={`${attraction.name}-image`} 
                     className="w-full h-full object-cover transition-transform duration-500 hover:scale-110"
-                  />
+                  /> */}
+                  <Skeleton className="w-full h-full" />
                 </div>
                 <CardContent className="p-6">
                   <div className="mb-2">
                     <span className="inline-block bg-primary/10 text-primary font-medium text-xs px-3 py-1 rounded-full">
-                      {attraction.category[language as keyof typeof attraction.category]}
+                      {attraction.shortDescription}
                     </span>
                   </div>
                   <h3 className="text-xl font-semibold text-gray-800">
-                    {attraction.name[language as keyof typeof attraction.name]}
+                    {attraction.name}
                   </h3>
                 </CardContent>
               </Card>
